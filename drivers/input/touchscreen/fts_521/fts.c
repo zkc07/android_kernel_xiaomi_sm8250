2cfacf333a9951a1adb82e68cdbdb2544335043b
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-18 01:45:49 -0500

    Revert "drivers: input: fts_521: Implement double_tap node"
    This reverts commit b40ef2cb733e5905f3f6e67f3bd28276de7fb198.
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -73,11 +73,6 @@
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 #include "../xiaomi/xiaomi_touch.h"
 #endif
-#ifdef GESTURE_MODE
-#ifdef CONFIG_TOUCHSCREEN_COMMON
-#include <linux/input/tp_common.h>
-#endif
-#endif
 #include "fts.h"
 #include "fts_lib/ftsCompensation.h"
 #include "fts_lib/ftsCore.h"
@@ -501,33 +496,6 @@ static ssize_t fts_feature_enable_show(struct device *dev,
 	return count;
 }
 #else
-
-#ifdef CONFIG_TOUCHSCREEN_COMMON
-static ssize_t double_tap_show(struct kobject *kobj,
-                               struct kobj_attribute *attr, char *buf)
-{
-    return sprintf(buf, "%d\n", fts_gesture_data.mode);
-}
-
-static ssize_t double_tap_store(struct kobject *kobj,
-                                struct kobj_attribute *attr, const char *buf,
-                                size_t count)
-{
-    int rc, val;
-
-    rc = kstrtoint(buf, 10, &val);
-    if (rc)
-    return -EINVAL;
-
-    fts_gesture_data.mode = !!val;
-    return count;
-}
-
-static struct tp_common_ops double_tap_ops = {
-    .show = double_tap_show,
-    .store = double_tap_store
-};
-#endif
 
 #ifdef GRIP_MODE
 /**
@@ -7500,14 +7468,11 @@ static int fts_probe(struct spi_device *client)
 	struct device_node *dp = client->dev.of_node;
 	int retval;
 	int skip_5_1 = 0;
-	u16 bus_type;
+	u16 bus_type;=
 #ifdef CONFIG_FACTORY_BUILD
 	int res = 0;
 	u8 gesture_cmd[6] = {0xA2, 0x03, 0x00, 0x00, 0x00, 0x03};
-#endif
-#ifdef CONFIG_TOUCHSCREEN_COMMON
-    int ret;
-#endif
+#endif=mplement double_tap node"
 
 	MI_TOUCH_LOGI(1, "%s %s: Probe start\n", tag, __func__);
 
@@ -7725,13 +7690,6 @@ static int fts_probe(struct spi_device *client)
 	mutex_init(&(info->input_report_mutex));
 #ifdef GESTURE_MODE
 	mutex_init(&gestureMask_mutex);
-#ifdef CONFIG_TOUCHSCREEN_COMMON
-    ret = tp_common_set_double_tap_ops(&double_tap_ops);
-    if (ret < 0) {
-        FTS_ERROR("%s: Failed to create double_tap node err=%d\n",
-                  __func__, ret);
-    }
-#endif
 #endif
 
 	spin_lock_init(&fts_int);
