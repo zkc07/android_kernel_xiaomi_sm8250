ec00366181fce80d1c09f0411171696ba4bfb054
Diaz1401 <reagor8161@outlook.com>
2022-03-06 10:32:12 +0700

    fs: exec: Fix incompatible parameter type
    * Miss merge at commit: e30a9e64555ee136dbfba6c3ef20838337d8efa6
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1825,7 +1825,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	would_dump(bprm, bprm.file);
+	would_dump(&bprm, bprm.file);
 
 	retval = exec_binprm(&bprm);
 	if (retval < 0)
