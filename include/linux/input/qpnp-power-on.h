fd12fc38c98391c2ebd6c1522f33f24059e94ed6
Diaz1401 <reagor8161@outlook.com>
2022-03-11 21:52:00 +0700

    arm64: Import XiaoMi changes
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -64,7 +64,8 @@ int qpnp_pon_wd_config(bool enable);
 int qpnp_pon_set_restart_reason(enum pon_restart_reason reason);
 bool qpnp_pon_check_hard_reset_stored(void);
 int qpnp_pon_modem_pwr_off(enum pon_power_off_type type);
-
+int qpnp_pon_is_ps_hold_reset(void);
+int qpnp_pon_is_lpk(void);
 #else
 
 static int qpnp_pon_system_pwr_off(enum pon_power_off_type type)
@@ -99,6 +100,16 @@ static inline bool qpnp_pon_check_hard_reset_stored(void)
 }
 
 static inline int qpnp_pon_modem_pwr_off(enum pon_power_off_type type)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_pon_is_ps_hold_reset(void)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_pon_is_lpk(void)
 {
 	return -ENODEV;
 }
